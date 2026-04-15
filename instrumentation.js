e7caad20769e17450e42696e108c4a885ff9eb43
vijayrajput1351 <vr9565899@gmail.com>
2026-04-15 12:41:38 +0530

    Updated instrumentation.js with version tagging and git config

--- instrumentation.js
+++ instrumentation.js
@@ -2,4 +2,9 @@ const tracker = require('@middleware.io/node-apm');
 tracker.track({
   serviceName: "vijay-nodejs",
   accessToken: "ammeyevndgfnuexkyplledmgwredjvyuyqum",
+  customResourceAttributes: {
+    "vcs.repository_url": "https://github.com/vijayrajput1351/qable",
+    "vcs.commit_sha": "98a709d",
+    "app.version": "1.0.0",
+  },
 });
